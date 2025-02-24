1e65e11f2c3f8ccafa1653ffde7e3cb93db7b1b5
EliOrozco <92065409+EliOrozco@users.noreply.github.com>
2025-02-24 00:05:39 -0600

    acomodado para trabajar
    renombrado variables y rediseño de interfaz

--- basic.gd
+++ basic.gd
@@ -6,51 +6,61 @@ var item_selected : int = 0
 var product_selected : int = 0
 var mod_selected : int = 0
 
+@onready var text_notif: RichTextLabel = $textNotif
+@onready var text_precio_final: RichTextLabel = $textPrecioFinal
+@onready var anadir: Button = $anadir
+@onready var eliminar: Button = $eliminar
+@onready var modificar: Button = $modificar
+@onready var lista_ticket: ItemList = $listaTicket
+@onready var lista_producto: ItemList = $listaProducto
+@onready var lista_mod: ItemList = $listaMod
+@onready var buscador: LineEdit = $buscador
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$LineEdit.grab_focus()
+	buscador.grab_focus()
+
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+func _process(_delta: float) -> void: #tiene que emular que ctrl está siendo mantenido presionado
 	pass
-
 
 func _on_button_pressed() -> void: #añadir
 	#get text
-	var lineEdit_Text : String = $LineEdit.text
-	item_selected = $ItemList2.get_selected_items()[0]
-	mod_selected = $ItemList3.get_selected_items()[0]
+	var lineEdit_Text : String = buscador.text
+	item_selected = lista_producto.get_selected_items()[0]
+	mod_selected = lista_mod.get_selected_items()[0]
 	
-	var item_text = $ItemList2.get_item_text(item_selected)
-	var mod_text = $ItemList3.get_item_text(mod_selected)
+	var item_text = lista_producto.get_item_text(item_selected)
+	var mod_text = lista_mod.get_item_text(mod_selected)
 	
 	if (lineEdit_Text == ""):
-		lineEdit_Text = "Añadido " + str(value) + item_text + mod_text
-	$ItemList.add_item(lineEdit_Text);
+		lineEdit_Text = "" + str(value) + item_text + mod_text
+	lista_ticket.add_item(lineEdit_Text);
 	value += 1
-	$RichTextLabel2.text = str(value)
+	text_precio_final.text = str(value)
 	send_notification("Producto añadido")
 
-
 func _on_button_2_pressed() -> void: #eliminar
-	$ItemList.select(item_selected - 1)
-	$ItemList.remove_item(item_selected)
+	lista_ticket.select(item_selected - 1)
+	lista_ticket.remove_item(item_selected)
 	item_selected -= 1
 	if (item_selected < 0):
 		item_selected = 0
-		$ItemList.select(0)
+		lista_ticket.select(0)
 	send_notification("Producto eliminado correctamente")
 
 func _on_button_3_pressed() -> void: #modificar
-	if ($ItemList.is_anything_selected()):
-		item_selected = $ItemList.get_selected_items()[0] #redundancia
-		$LineEdit.text = $ItemList.get_item_text(item_selected)
+	if (lista_ticket.is_anything_selected()):
+		item_selected = lista_ticket.get_selected_items()[0] #redundancia
+		buscador.text = lista_ticket.get_item_text(item_selected)
 
-func _on_item_list_item_activated(index: int) -> void: #redundacia, este es para clicks doble
+func _on_item_list_item_activated(index: int) -> void: #redundacia, este es para clicks doble, no borrar
 	pass
 
 func _on_item_list_item_selected(index: int) -> void:
-	item_selected = $ItemList.get_selected_items()[0]
+	item_selected = lista_ticket.get_selected_items()[0]
 
 func send_notification(notif: String):
-	$RichTextLabel.text = notif
+	text_notif.text = notif
